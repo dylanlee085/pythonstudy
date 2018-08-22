5945eef79beb2b4c4664456e37822fee2f5ad5a8
lee <ljp_20131201@163.com>
2018-08-23 00:40:06 +0800

    spider_code.py

--- spider/spider_code.py
+++ spider/spider_code.py
@@ -7,57 +7,59 @@ import sys
 import re
 reload(sys)
 sys.setdefaultencoding('utf-8')
+import xproxy
+import time
 
 
 def get_province(url):
+    proxy = xproxy.get_random_ip()
+    print proxy
     headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/68.0.3440.106 Safari/537.36'}
-    response = requests.get(url,headers=headers)
+    response = requests.get(url,headers=headers,proxies = proxy)
     soup = BeautifulSoup(response.text.encode(response.encoding), 'lxml')
     content = content = soup.find_all('tr', class_='provincetr')
-    pro_names = []
-    pro_urls = []
     for i in content:
         for i in i.find_all('a'):
             pattern = re.compile('<a.*?>(.*?)<br/></a>', re.S)
-            name =re.findall(pattern,str(i))[0]
-            pro_names.append(name)
+            pro_names =re.findall(pattern,str(i))[0]
             prefix = i.get('href')
-            fullurl = 'http://www.stats.gov.cn/tjsj/tjbz/tjyqhdmhcxhfdm/2017/' + prefix
-            pro_urls.append(fullurl)
-    return pro_names, pro_urls
+            pro_urls = 'http://www.stats.gov.cn/tjsj/tjbz/tjyqhdmhcxhfdm/2017/' + prefix
+            print pro_names, pro_urls
+            yield pro_names, pro_urls
+            
 
 
 def get_all_City(pro_names,pro_urls):
-    for name, url in zip(pro_names, pro_urls):
-        headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/68.0.3440.106 Safari/537.36'}
-        response = requests.get(url,headers=headers)
-        html = response.text.encode(response.encoding)
-#        html = response.text
-#        soup = BeautifulSoup(response.text.encode(response.encoding), 'lxml')
-#        content = content = soup.find_all('tr', class_='citytr')
-        city_names = []
-        city_urls = []
-        city_codes = []
-#        for i in content:
-        pattern = re.compile('<tr class=.*?><td><a href=(.*?)>(.*?)</a></td><td><a.*?>(.*?)</a></td></tr>', re.S)
-        for i in re.findall(pattern,html):
-#            print i
-            prefix = i[0]
-            code = i[1]
-#        city_codes.append(code)
-            name = i[2]
-            print name
-#        city_names.append(name)
+    proxy = xproxy.get_random_ip()
+    print proxy
+#    for name, url in zip(pro_names, pro_urls):
+    headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/68.0.3440.106 Safari/537.36'}
+    response = requests.get(url,headers=headers,proxies = proxy)
+    response.encoding = 'gb2312'
+    html = response.text
+    print html
+    city_names = []
+    city_urls = []
+    city_codes = []
+    infos = []
+    pattern = re.compile('<tr class=.*?><td><a href=(.*?)>(.*?)</a></td><td><a.*?>(.*?)</a></td></tr>', re.S)
+    for i in re.findall(pattern,html):
+        print i
+        code = i[1]
+        city_codes.append(code)
+        name = i[2]
+        city_names.append(name)
+        prefix = i[0]
         fullurl = 'http://www.stats.gov.cn/tjsj/tjbz/tjyqhdmhcxhfdm/2017/' + prefix
-#        city_urls.append(fullurl)
-#      print city_codes, city_names, city_urls
-#        print city_codes, city_names, city_urls
-#        print code, name, fullurl
+        city_urls.append(fullurl)
+        info = city_codes, city_names, city_urls
+        infos.append(info)
+    print infos
 
 
 
 if __name__ == "__main__":
     url = 'http://www.stats.gov.cn/tjsj/tjbz/tjyqhdmhcxhfdm/2017/index.html'
-    pro_names, pro_urls = get_province(url)
-    get_all_City(pro_names,pro_urls)
-  
+    for pro_names, pro_urls in  get_province(url):
+        get_all_City(pro_names,pro_urls)
+        time.sleep(5)
